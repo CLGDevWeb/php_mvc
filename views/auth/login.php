7a8a7db540947b3e26dae77b5b3cc5f98b0f208e
unknown <clgdevweb@gmail.com>
2020-11-30 11:37:28 +0100

    validator wip

--- views/auth/login.php
+++ views/auth/login.php
@@ -1,3 +1,20 @@
+<?php if (isset($_SESSION['errors'])): ?>
+
+<?php foreach($_SESSION['errors'] as $errorsArray): ?>
+    <?php foreach($errorsArray as $errors): ?>
+        <div class="alert alert-danger">
+            <?php foreach($errors as $error): ?>
+                <li><?= $error ?></li>
+            <?php endforeach ?>
+        </div>
+    <?php endforeach ?>
+
+<?php endforeach ?>
+
+<?php endif ?>
+
+<?php session_destroy(); ?>
+
 <h1>Se connecter</h1>
 
 <form action="/php_mvc/login" method="POST">
