19012cb1ed6450c94a9077a271d7d8e05fcba436
CLGDevWeb <clgdevweb@gmail.com>
2021-04-05 15:19:46 +0200

    some tests on layout WIP

--- views/posts/index.php
+++ views/posts/index.php
@@ -1,25 +1,25 @@
-<div class="relative flex flex-1 bg-gray-900 pt-16 pb-20 px-4 sm:px-6 lg:pt-24 lg:pb-28 lg:px-8">
+<div class="relative flex flex-1 pt-16 pb-20 px-4 sm:px-6 lg:pt-24 lg:pb-28 lg:px-8">
     <div class="absolute inset-0">
-        <div class="bg-gray-400 h-1/3 sm:h-2/3"></div>
+        <div class=" h-1/3 sm:h-2/3"></div>
     </div>
     <div class="relative max-w-7xl mx-auto">
         <div class="text-center">
             <h2 class="text-3xl tracking-tight font-extrabold text-gray-200 sm:text-4xl">
-                From the blog
+                Some articles
             </h2>
             <p class="mt-3 max-w-2xl mx-auto text-xl text-gray-500 sm:mt-4">
                 Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ipsa libero labore natus atque, ducimus sed.
             </p>
         </div>
-        <section class="mt-12 max-w-lg mx-auto grid gap-5 lg:grid-cols-3 lg:max-w-none">
+        <section class="mt-12 max-w-lg mx-auto grid gap-5 lg:grid-cols-3 lg:max-w-none overflow-y-auto">
             <?php foreach ($params['posts'] as $post) : ?>
-                <article class="flex flex-col rounded-lg shadow-lg overflow-hidden">
+                <article class="flex flex-col bg-gray-300 rounded-lg shadow-lg overflow-hidden">
                     <div class="flex-shrink-0">
                         <img class="h-48 w-full object-cover" src="https://images.unsplash.com/photo-1547586696-ea22b4d4235d?ixlib=rb-1.2.1&ixqx=4bxs01cNtp&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1679&q=80" alt="">
                     </div>
-                    <div class="flex-1 bg-white p-6 flex flex-col justify-between">
+                    <div class="flex-1 p-6 flex flex-col justify-between">
                         <div class="flex-1">
-                            <p class="text-sm font-medium text-indigo-600">
+                            <p class="text-sm font-medium text-blue-600">
                                 <?php foreach ($post->getTags() as $tag) : ?>
                                     <a href="/php_mvc/tags/<?= $tag->id ?>" class="hover:underline">
                                         <?= $tag->name ?>
@@ -30,7 +30,7 @@
                                 <p class="text-xl font-semibold text-gray-900">
                                     <?= $post->title ?>
                                 </p>
-                                <p class="mt-3 text-base text-gray-500">
+                                <p class="mt-3 text-base text-gray-700">
                                     <?= $post->getExcerpt(); ?>
                                 </p>
                             </a>
